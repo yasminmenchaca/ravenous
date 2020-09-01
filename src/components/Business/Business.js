cdcb5483167bbc1bfd72c3674952c65f13760dd9
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-09-01 14:23:51 -0500

    exercise complete

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Business from 'Business.css';
+import './Business.css';
 
 const business = {
     imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
