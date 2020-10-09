26ed8ba31f1132efa734ea2f4f24c10141efe76f
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-10-09 08:50:45 -0500

    ---

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -76,6 +76,7 @@ class SearchBar extends React.Component {
                     <input placeholder="Where?" onChange={this.handleLocationChange}/>
                 </div>
                 <div className="SearchBar-submit">
+                    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
                     <a onClick={this.handleSearch}>Let's Go</a>
                 </div>
             </div>
